b6eacb0d888cca11f793b550fbc35bb73af165b7
nikitadubino <nikita.dubino@softswiss.com>
2023-09-18 18:57:35 +0400

    fix

---
+++ Homework/homework_lesson_03/main.py
@@ -0,0 +1,42 @@
+secondsInDay = 60 * 60 * 24
+secondsInHour = 60 * 60
+secondsInMinute = 60
+
+
+def secondsToDays(seconds):
+    return seconds // secondsInDay
+
+
+def secondsToHours(seconds):
+    return seconds // secondsInHour
+
+
+def secondsToMinutes(seconds):
+    return seconds // secondsInMinute
+
+
+def secondsToSeconds(seconds):
+    return seconds % secondsInMinute
+
+
+formatters = (
+    (secondsToDays, secondsInDay),
+    (secondsToHours, secondsInHour),
+    (secondsToMinutes, secondsInMinute),
+    (secondsToSeconds, 1)
+)
+
+
+def formatSecondsDuration(durationInSeconds):
+    seconds = durationInSeconds
+    units = []
+
+    for formatter, multiplier in formatters:
+        unit = formatter(seconds)
+        seconds -= unit * multiplier
+        units.append(str(unit).zfill(2))
+
+    return ':'.join(units)
+
+
+print(formatSecondsDuration(3600))
