205ab20256c6661122c119f70df099d0db9e555e
nikitadubino <nikita.dubino@softswiss.com>
2023-10-22 17:40:32 +0400

    Homework

--- practice/lesson3/test2.py
+++ practice/lesson3/test2.py
@@ -1,6 +1,7 @@
-def example():
-    print(1)
-    return 2
-    print(3)
+from typing import List
 
-example()
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 + 32.00
+        return [kelvin, fahrenheit]
